13aacf93c9e5832a1b77c1dc850fa8a3e5ae8021
Eric Gruber <Egruber@nuvotechnologies.com>
2016-11-24 13:17:29 -0500

    Updated the output because Ally asked me to do it

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def start
-    render html: "Words because Reasons" 
+    render html: "I wrote this over because Ally anted me to do so." 
   end
 
 end
