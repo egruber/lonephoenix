f1234b4cb244f5cf202eed230720bd1f1fe5eeeb
Eric Gruber <Egruber@nuvotechnologies.com>
2016-11-24 13:20:57 -0500

    Another flipping typo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def start
-    render html: "I wrote this over because Ally anted me to do so." 
+    render html: "I wrote this over because Ally wanted me to do so." 
   end
 
 end
