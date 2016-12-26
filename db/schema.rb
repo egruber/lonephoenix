7546bc644240c653ebca134b88b43b81beb7d865
Eric Gruber <lonephoenix@gmail.com>
2016-12-26 16:56:27 -0500

    Add subject to the post DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161226213924) do
+ActiveRecord::Schema.define(version: 20161226215229) do
 
   create_table "posts", force: :cascade do |t|
     t.text     "content"
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "subject"
   end
 
   create_table "users", force: :cascade do |t|
