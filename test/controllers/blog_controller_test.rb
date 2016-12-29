0739cda1b3aa9a9fa3e86f0345dc2b1c2f04ea87
Eric Gruber <lonephoenix@gmail.com>
2016-12-29 16:19:55 -0500

    Create title tests and add a Layout file

--- test/controllers/blog_controller_test.rb
+++ test/controllers/blog_controller_test.rb
@@ -6,16 +6,19 @@ class BlogControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get blog_home_url
     assert_response :success
+    assert_select "title","Lonephoenix - Home"
   end
 
   test "should get about" do
     get blog_about_url
     assert_response :success
+    assert_select "title","Lonephoenix - About"
   end
 
   test "should get games" do
   	get blog_games_url
   	assert_response :success
+  	assert_select "title","Lonephoenix - Games"
   end
 
 end
