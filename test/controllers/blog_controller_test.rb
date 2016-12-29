e325775dd80f9320b4a2444b12e92b43b3d31b8a
Eric Gruber <lonephoenix@gmail.com>
2016-12-29 16:42:36 -0500

    Create a good layout and stopped repeating myself in the views

--- test/controllers/blog_controller_test.rb
+++ test/controllers/blog_controller_test.rb
@@ -2,23 +2,27 @@ require 'test_helper'
 
 class BlogControllerTest < ActionDispatch::IntegrationTest
   
+  def setup
+  	@title_suffix = "| Lonephoenix"
+  end
+
   # Existance tests 
   test "should get home" do
     get blog_home_url
     assert_response :success
-    assert_select "title","Lonephoenix - Home"
+    assert_select "title","Home #{@title_suffix}"
   end
 
   test "should get about" do
     get blog_about_url
     assert_response :success
-    assert_select "title","Lonephoenix - About"
+    assert_select "title","About #{@title_suffix}" 
   end
 
   test "should get games" do
   	get blog_games_url
   	assert_response :success
-  	assert_select "title","Lonephoenix - Games"
+  	assert_select "title","Games #{@title_suffix}"
   end
 
 end
