efb0f9cc34b5055c7844b770ca484fefe9c3db45
Eric Gruber <lonephoenix@gmail.com>
2016-12-29 15:42:55 -0500

    More reverts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'foo/help'
-
   get 'statics/home'
   resources :posts
   resources :users
--- test/controllers/foo_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class FooControllerTest < ActionDispatch::IntegrationTest
-  test "should get help" do
-    get foo_help_url
-    assert_response :success
-  end
-
-end
