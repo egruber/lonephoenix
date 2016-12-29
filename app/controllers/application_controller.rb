f68352146e28126b8c0f9f97765a62361c21e833
Eric Gruber <lonephoenix@gmail.com>
2016-12-29 16:44:51 -0500

    Remove the old Start action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def start
-    render html: "I wrote this over because Ally wanted me to do so." 
-  end
-
 end
