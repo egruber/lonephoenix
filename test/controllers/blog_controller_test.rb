06bef4e7bdede5a827c868133f6409f4e0bbbd80
Eric Gruber <lonephoenix@gmail.com>
2016-12-29 16:02:58 -0500

    Add test for games section

--- test/controllers/blog_controller_test.rb
+++ test/controllers/blog_controller_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class BlogControllerTest < ActionDispatch::IntegrationTest
+  
+  # Existance tests 
   test "should get home" do
     get blog_home_url
     assert_response :success
@@ -9,6 +11,11 @@ class BlogControllerTest < ActionDispatch::IntegrationTest
   test "should get about" do
     get blog_about_url
     assert_response :success
+  end
+
+  test "should get games" do
+  	get blog_games_url
+  	assert_response :success
   end
 
 end
